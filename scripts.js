5156e4e67a3589b58d970930850a5a8afefcaffd
Vedant <127001567+VedantSur@users.noreply.github.com>
2025-01-30 08:00:51 +0000

    arrow

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-// Check and apply the user's system preferred theme (dark or light) on page load
 document.addEventListener('DOMContentLoaded', () => {
+   
     const toggleButton = document.getElementById('theme-toggle');
     const currentTheme = localStorage.getItem('theme');
 
-    // Check the system theme preference and apply it if not overridden
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    // Apply the theme on page load
     if (currentTheme) {
         document.body.classList.add(currentTheme);
         updateIcon(currentTheme);
@@ -18,9 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
         updateIcon('light-mode');
     }
 
-    // Event listener for the theme toggle button
     toggleButton.addEventListener('click', () => {
-        // Toggle between dark and light mode
+        
         if (document.body.classList.contains('dark-mode')) {
             document.body.classList.remove('dark-mode');
             document.body.classList.add('light-mode');
@@ -31,19 +28,29 @@ document.addEventListener('DOMContentLoaded', () => {
             updateIcon('dark-mode');
         }
 
-        // Save the current theme to localStorage
+       
         localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode');
+    });
+
+    const arrow = document.querySelector('.scroll-down-arrow');
+
+  
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            arrow.classList.add('hidden');
+        } else {
+            arrow.classList.remove('hidden'); 
+        }
     });
 });
 
-// Function to update the icon on the toggle button
 function updateIcon(theme) {
     const icon = document.querySelector('#theme-toggle i');
     if (theme === 'dark-mode') {
         icon.classList.remove('fa-sun');
-        icon.classList.add('fa-moon'); // Change icon to moon for dark mode
+        icon.classList.add('fa-moon'); 
     } else {
         icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun'); // Change icon to sun for light mode
+        icon.classList.add('fa-sun');
     }
 }
